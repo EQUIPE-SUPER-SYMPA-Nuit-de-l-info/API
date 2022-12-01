d82da4869fc441c3e5e3f2010a14772b3c472d81
erisson <aime.risson.1@gmail.com>
2022-12-01 23:29:34 +0100

    cors

--- app/main.py
+++ app/main.py
@@ -4,6 +4,17 @@ from .endpoints.experience import experience_router
 
 
 app = FastAPI()
+
+
+origins = []
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(experience_router)
 
 @app.get("/")
