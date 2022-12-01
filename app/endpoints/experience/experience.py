6de089acb8be770a2abe1624e6383460f6633e17
erisson <aime.risson.1@gmail.com>
2022-12-02 00:03:15 +0100

    modif endpoint

--- app/endpoints/experience/experience.py
+++ app/endpoints/experience/experience.py
@@ -1,15 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from typing import List
 from app.core.models.experience import Experience
+from sqlalchemy.orm import Session
+from app.core.postgres import crud
+from app.core.postgres.main import get_db
 router = APIRouter()
 
 experiences = []
 
 @router.post('/experience', tags=['experience'])
-async def creer_experience(experience: Experience):
-    experiences.append(experience)
+def creer_experience(experience: Experience, db: Session = Depends(get_db)):
+    crud.creer_experience(db, experience.titre, experience.contenu)
     return "ok"
 
-@router.get('/experiences', tags=['experience'], response_model=List[Experience])
-async def get_experiences():
-    return experiences
+@router.get('/experiences', tags=['experience'])
+def get_experiences(db: Session = Depends(get_db)):
+    return crud.get_experiences(db)
